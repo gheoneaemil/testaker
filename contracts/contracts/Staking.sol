476de7c9e917a19972e4d89b9bdb6f3b14cbcd4d
DIIMIIM <emilian@stater.co>
2022-07-12 15:38:29 +0300

    generated new types

--- contracts/contracts/Staking.sol
+++ contracts/contracts/Staking.sol
@@ -7,7 +7,7 @@ contract Staking {
     using Address for address;
 
     // Parameters
-    uint128 public constant VALIDATOR_THRESHOLD = .00001 ether;
+    uint128 public constant VALIDATOR_THRESHOLD = 1 ether;
 
     // Properties
     address[] public _validators;
